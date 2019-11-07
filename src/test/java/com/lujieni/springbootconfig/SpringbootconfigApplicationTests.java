6bb8da304ece643d5709a3e0c84c1a7e48e7b746
admin <982584989@qq.com>
2019-11-07 13:27:55 +0800

    代码提交

--- src/test/java/com/lujieni/springbootconfig/SpringbootconfigApplicationTests.java
+++ src/test/java/com/lujieni/springbootconfig/SpringbootconfigApplicationTests.java
@@ -4,13 +4,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootconfigApplicationTests {
 
+    /**
+     * 利用assert进行判空校验
+     */
     @Test
-    public void contextLoads() {
+    public void testAssert() {
+        String a = "";
+        String b = "a";
+        Integer c = 1;
+        Long d = 5L;
+        Object[] checkItems = {a,b,c,d};
+        Assert.noNullElements(checkItems,"字段不能为null");
     }
-
 }
