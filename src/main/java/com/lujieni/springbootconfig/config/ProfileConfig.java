a5e648f7afed9e4bb8d54c094578f7054cad75d9
181268 <lujieni1017>
2021-10-19 15:35:02 +0800

    删除代码

--- src/main/java/com/lujieni/springbootconfig/config/ProfileConfig.java
+++ src/main/java/com/lujieni/springbootconfig/config/ProfileConfig.java
@@ -1,16 +1,31 @@
 package com.lujieni.springbootconfig.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
+/**
+ * @Description: Environment对象可以获取.properties文件中的参数,如果没有就为空不会报错
+ * @return:
+ * @Author: lujieni
+ * @Date: 2021/10/19
+ */
 @Configuration
 @Slf4j
 public class ProfileConfig {
+
+    @Autowired
+    private Environment environment;
+
     @Bean
-    public String readProfile(@Value("${env}") String env){
-        log.info("env is {}",env);
+    public String readProfile(@Value("${name}") String env){
+        log.info("name is {}",env);
+        log.info("name123 is "+environment.getProperty("name123"));
         return env;
     }
+
+
 }
